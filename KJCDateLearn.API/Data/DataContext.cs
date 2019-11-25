bbe0a047d607bd626d962c12593ada8a3a5d3352
Ken Cartwright <kennethjcartwright@gmail.com>
2019-11-25 11:44:25 -0500

    initial commit

---
+++ KJCDateLearn.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using KJCDateLearn.API.Models; 
+
+namespace KJCDateLearn.API.Data
+{
+    // test
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ KJCDateLearn.API/Models/Value.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace KJCDateLearn.API.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Value()
+        {
+        }
+    }
+}
